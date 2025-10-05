2e2e6aaa5589dc8c3a93d5c8a0e6ae5c2b5be643
Kimberly Santana <Kim101010ss@gmail.com>
2025-10-05 17:03:45 -0600

    Update Trafico class implementation and project configuration files

---
+++ Lab0310/src/poo/Trafico.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package poo;
+
+/**
+ *
+ * @author Kimberly Santana
+ */
+public class Trafico {
+
+    private String nombre;
+    private int cantidadCarros;
+
+    public Trafico(String nombre, int cantidadCarros) {
+        this.nombre = nombre;
+        this.cantidadCarros = cantidadCarros;
+    }
+
+    public String obtenerEstadoTrafico(int cantidad) {
+        if (cantidad <= 3) {
+            return "tiene un flujo ligero";
+        }
+        if (cantidad <= 5) {
+            return "tiene un flujo moderado";
+        }
+        if (cantidad <= 8) {
+            return "esta comenzando a congestionarse";
+        }
+        if (cantidad <= 11) {
+            return "se esta gerenando un embotellamiento en la carretera.";
+        }
+        return " hay mucho embotellamiento";
+
+    }
+
+    public void verificarEstado() {
+        //int cantidad = this.cantidadCarros();
+        String estado = obtenerEstadoTrafico(cantidadCarros);
+        System.out.printf("La carretera %s %s\n", nombre, estado);
+    }
+
+}
